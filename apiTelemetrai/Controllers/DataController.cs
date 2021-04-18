1a01e08b677b47677d5c3c44bde756391c295180
Matias Torrico <a2016114304@estudiantes.upsa.edu.bo>
2021-04-18 18:10:22 -0400

    Api completa

---
+++ apiTelemetrai/Controllers/DataController.cs
@@ -0,0 +1,43 @@
+﻿using apiTelemetrai.Models;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiTelemetrai.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+
+            string connectionString = "Endpoint=sb://queuematias.servicebus.windows.net/;SharedAccessKeyName=EnviarEscuchar;SharedAccessKey=lYoAp1ZH4bl82XopQWgqWp5v2b1r8jF++31YX9qqF5U=;EntityPath=telemetria";
+            string queueName = "telemetria";
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ apiTelemetrai/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiTelemetrai.Models
+{
+    public class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDateTime { get; set; }
+        [Required]
+        public string Temperatura { get; set; }
+        [Required]
+        public string Humedad { get; set; }
+    }
+}
