c67faf72b375a349a6041c51b9efd038968e3808
Matias Torrico <a2016114304@estudiantes.upsa.edu.bo>
2021-04-18 18:41:02 -0400

    Consumidor funcionando

---
+++ fncLector/Function1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using fncLector.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fncLector
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task RunAsync(
+            [ServiceBusTrigger("telemetria", Connection = "MyConn")] string myQueueItem,
+            [CosmosDB(databaseName: "dbTelemetria", collectionName: "Datos", ConnectionStringSetting = "strCosmos")] IAsyncCollector<object> datos,
+            ILogger log)
+        {
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<Data>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"No fue posible insertar datos: {e.Message}");
+            }
+        }
+    }
+}
---
+++ fncLector/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace fncLector.Models
+{
+    class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDateTime { get; set; }
+        [Required]
+        public string Temperatura { get; set; }
+        [Required]
+        public string Humedad { get; set; }
+    }
+}
